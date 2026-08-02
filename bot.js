03a34dc719765ac18799a7f542edb0704fa04698
lip0re <luiwnlz@gmail.com>
2026-08-01 19:56:03 -0500

    4

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
-
+const app = express();
 
 // =====================
 // Cargar estadísticas
@@ -31,6 +31,16 @@ const app = express();
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en puerto ${PORT}`);
+});
 
 
 app.get("/stats", (req, res) => {
